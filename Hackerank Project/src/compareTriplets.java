d837827e9248706b3102f759f41a55c4ab1f6512
Csaintilienc1 <39477225+Csaintilienc1@users.noreply.github.com>
2019-01-28 14:16:40 -0500

    Add files via upload

---
+++ Hackerank Project/src/compareTriplets.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class compareTriplets {
+
+	
+	
+	
+	
+	 // Complete the compareTriplets function below.
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+       int aPoints=0;
+       int bPoints=0;
+       int[] scoreArray= new int[2]; 
+
+       for(int i= 0; i<a.size(); i++) {
+    	   for(int n=0; n<b.size(); n++  ) {
+    		   
+    		   
+    	   }
+       }
+	
+
+
+
+             
+    }
+}
